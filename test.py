d393a1660140a190b3bd798ab5ee43b3617a4b76
bengilbert12 <bgyonce@gmail.com>
2022-04-29 20:32:14 -0400

    25 reset players holding die after round end

--- test.py
+++ test.py
@@ -9,3 +9,11 @@ def test_diversity():
     for player in range(test_game.player_count):
         score = test_game.scorecard[player]
         assert score["wins"] != 100 and score["ties"] != 100
+
+def test_die_reset():
+    test_game = Game(4)
+
+    test_game.play_rounds(2)
+
+    for player in test_game.players:
+        assert len(player.holding_die) <= 6 and len(player.rollable_die) <= 6
